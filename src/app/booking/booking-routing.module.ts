3d746d8771a9164751af1d9b0557c2c75c458cc4
casparsylwan <casparsylwan@gmail.com>
2020-12-22 18:38:35 +0100

    Added more components

--- src/app/booking/booking-routing.module.ts
+++ src/app/booking/booking-routing.module.ts
@@ -2,8 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { BookingComponent } from './booking.component';
+import { PaymentComponent } from './steps/payment/payment.component';
+import { SeatComponent } from './steps/seat/seat.component';
 
-const routes: Routes = [{ path: '', component: BookingComponent }];
+const routes: Routes = [
+  { path: '', component: BookingComponent },
+  { path: 'seat', component: SeatComponent},
+  { path: 'payment', component: PaymentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/booking/booking.module.ts
+++ src/app/booking/booking.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { BookingRoutingModule } from './booking-routing.module';
 import { BookingComponent } from './booking.component';
+import { DateComponent } from './steps/date/date.component';
+import { SeatComponent } from './steps/seat/seat.component';
+import { PaymentComponent } from './steps/payment/payment.component';
 
 
 @NgModule({
-  declarations: [BookingComponent],
+  declarations: [BookingComponent, DateComponent, SeatComponent, PaymentComponent],
   imports: [
     CommonModule,
     BookingRoutingModule
